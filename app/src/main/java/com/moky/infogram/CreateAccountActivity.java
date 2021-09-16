ba6ff4e89348784e7001e46782e73ad435fcda0c
v-mari <violettrujillo565@gmail.com>
2021-09-16 05:45:57 -0400

    card view

--- app/src/main/java/com/moky/infogram/CreateAccountActivity.java
+++ app/src/main/java/com/moky/infogram/CreateAccountActivity.java
@@ -3,12 +3,29 @@ package com.moky.infogram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    private Toolbar supportActionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        showToolBar(getString(R.string.title_crear_cuenta), true);
+    }
+
+    public void showToolBar(String titulo, boolean botonSubir) {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+
+    }
+
+    public void setSupportActionBar(Toolbar supportActionBar) {
+        this.supportActionBar = supportActionBar;
     }
 }
--- app/src/main/java/com/moky/infogram/LoginActivity.java
+++ app/src/main/java/com/moky/infogram/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.moky.infogram;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,6 +14,8 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
     public void goCreateAccount(View view){
-        System.out.println("--------------> go to create account");
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+
+    startActivity(intent);
     }
 }
